3ec829022f21405d8d7783581a7b07ebd2b2f870
Guslington <guslington@gmail.com>
2019-01-15 16:46:56 +1100

    what about 1.11.471

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -12,7 +12,7 @@
  )
  ************************************/
 
-@Grab(group='com.amazonaws', module='aws-java-sdk-ecr', version='1.11.476')
+@Grab(group='com.amazonaws', module='aws-java-sdk-ecr', version='1.11.471')
 
 import com.amazonaws.services.ecr.*
 import com.amazonaws.services.ecr.model.*
