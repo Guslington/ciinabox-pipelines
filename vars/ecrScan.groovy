769167dac49ec3bd15e3a2aed3850fd886612d35
Guslington <guslington@gmail.com>
2019-10-30 15:42:53 +1100

    bump sdk to 1.11.662

--- vars/ecrScan.groovy
+++ vars/ecrScan.groovy
@@ -12,7 +12,7 @@
  )
  ************************************/
 
-@Grab(group='com.amazonaws', module='aws-java-sdk-ecr', version='1.11.661')
+@Grab(group='com.amazonaws', module='aws-java-sdk-ecr', version='1.11.662')
 
 import com.amazonaws.services.ecr.*
 import com.amazonaws.services.ecr.model.*
@@ -26,6 +26,42 @@ def call(body) {
     triggerScan(ecr,config)
   }
   
+  waitForScanResults(ecr,config)
+  displayScanResults(ecr, config)
+}
+
+@NonCPS
+def displayScanResults(ecr,config) {
+  ImageScanFindings findings = getScanResults(ecr,config).getImageScanFindings()
+}
+
+@NonCPS
+def waitForScanResults(ecr,config) {
+  def status = getScanResults(ecr,config)
+    .getImageScanStatus().getStatus()
+  while(status == 'IN_PROGRESS') {
+    println 'waiting for image scan to complete'
+    Thread.sleep(5000)
+    status = getScanResults(ecr,config)
+      .getImageScanStatus().getStatus()
+    if (status == 'COMPLETE') {
+      println 'image scan completed'
+      break
+    } else {
+      throw new Exception("image scan failed to complete. ${status}")
+    }
+  }
+}
+
+@NonCPS
+def getScanResults(ecr,config) {
+  def imageId = new ImageIdentifier().withImageTag(config.tag)
+  def request = new DescribeImageScanFindingsRequest()
+    .withRepositoryName(config.image)
+    .withRegistryId(config.accountId)
+    .withImageId(imageId) 
+  DescribeImageScanFindingsResult result = ecr.describeImageScanFindings(request)
+  return result
 }
 
 @NonCPS
