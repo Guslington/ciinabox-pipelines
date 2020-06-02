a917cea949d0f64e8ab5ae57b034c2066fd999c3
Guslington <guslington@gmail.com>
2020-06-02 14:33:14 +1000

    basic ecs-cli with auto discovery

---
+++ vars/ciinaboxEcsTask.groovy
@@ -0,0 +1,60 @@
+/***********************************
+ciinaboxEcsTask DSL
+
+runs a tempory build or testing task on the ciinabox cluster using the ecs-cli
+
+example usage
+ciinaboxEcsTask(
+  action: 'up|down' // (required, starts or stops the task)
+  composeFile: 'docker-compose.yml', // (optional, defaults to docker-compose.yml in the workspace directory)
+  ecsParams: 'ecs-params.yml', // (optional, https://docs.aws.amazon.com/AmazonECS/latest/developerguide/cmd-ecs-cli-compose-ecsparams.html)
+  cluster: 'my-ecs-cluster', // (optional, defaults to the jenkins ecs cluster)
+  executionRole: 'role', // (optional, defaults to the jenkins execution role)
+  memory: 1024, // (optional, defaults to 1GB)
+  cpu: 512, // (optional, defaults to 0.5GB)
+  subnet: 'subnet-123456789', // (optional, defaults to the jenkins subnet)
+  securityGroup: 'sg-123456789', // (optional, defaults to the jenkins security group)
+  region: 'ap-southeast-2' // (optional, defaults to current region if in AWS)
+)
+
+************************************/
+
+import com.base2.ciinabox.InstanceMetadata
+import com.base2.ciinabox.GetInstanceDetails
+import com.base2.ciinabox.GetEcsContainerDetatils
+
+def call(body) {
+  def config = body
+  
+  if !(config.action) || (config.action ==~ /^(up|down)$/){
+    error("action param must be supplied with the value of `up` or `down`")
+  }
+  
+  def id = UUID.randomUUID().toString()
+  
+  def composeFile = config.get('composeFile', 'docker-compose.yml')
+  
+  def metadata = new InstanceMetadata()
+  // if the node is a ec2 instance using the ec2 plugin
+  def instanceId = env.NODE_NAME.find(/i-[a-zA-Z0-9]*/)
+    
+  if (!instanceId) {
+    instanceId = metadata.instanceId()
+  }
+
+  def instance = new GetInstanceDetails(metadata.region(), instanceId)
+  
+  def region = config.get('region', metadata.region())
+  def subnet = config.get('subnet', instance.subnet())
+  def securityGroup = config.get('securityGroup', instance.securityGroup())
+  
+  def task = new GetEcsContainerDetatils(metadata.region(), env)
+  
+  def instanceProfile = config.get('executionRole', task.executionRole())
+  def cluster = config.get('cluster', task.cluster())
+  
+  def cpu = config.get('cpu', 512)
+  def cpu = config.get('cpu', 1024)
+  
+  sh "ecs-cli compose --project-name ${id} --file ${composeFile} ${config.action} --launch-type FARGATE --cluster ${cluster}"
+}
